3ec4f4d18919e97d4d2d50a16daaa2318b84e342
highpriesst <ertansofia@gmail.com>
2023-03-30 19:33:44 +0300

    fresh

--- store.js
+++
@@ -1,8 +0,0 @@
-// import { configureStore } from "@reduxjs/toolkit";
-// import { getUser, updateUser, removeUser } from "./utils";
-
-// const store = configureStore({ reducer: updateUser, removeUser, getUser });
-
-// console.log(store.getState());
-
-// export default store;
--- utils/index.js
+++
@@ -1,4 +0,0 @@
-//ACTION TYPES
-export const UPDATE_USER = "UPDATE_USER";
-export const REMOVE_USER = "REMOVE_USER";
-export const GET_USERS = "GET_USERS";
