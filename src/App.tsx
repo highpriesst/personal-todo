8385726e61f3ae1c8490078f5de85fbba106e6af
highpriesst <ertansofia@gmail.com>
2023-04-01 23:00:07 +0300

    Building the front end for according to server, fixed axios connection error due to Brave's blocking the http requests

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import TodoList from "./components/TodoList";
+import TodoForm from "./components/TodoForm";
+
+function App() {
+  return (
+    <div className="App">
+      <h2>TODO APP</h2>
+      <TodoList />
+      <TodoForm />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/TodoForm.tsx
@@ -0,0 +1,8 @@
+import React, { SyntheticEvent, useState } from "react";
+import axios from "axios";
+
+const TodoForm = () => {
+  <h1>todoform</h1>;
+};
+
+export default TodoForm;
