6d23a55aa83ce2004a8274df5e83a5845615bdf4
highpriesst <ertansofia@gmail.com>
2023-04-03 17:22:41 +0300

    Created a new file to use custom functions in different files

---
+++ src/utils/helper.ts
@@ -0,0 +1,38 @@
+import axios, { AxiosResponse } from "axios";
+import { ApiDataType, ITodo } from "../types/todo.types";
+
+const baseUrl: string = "http://localhost:3000";
+
+export const getTodos = async (): Promise<AxiosResponse<ApiDataType>> => {
+  try {
+    const todos: AxiosResponse<ApiDataType> = await axios.get(
+      baseUrl + "/api/todos"
+    );
+    console.log(todos.data.todos);
+
+    return todos;
+  } catch (error) {
+    throw new Error();
+  }
+};
+
+export const addTodo = async (
+  formdata: ITodo
+): Promise<AxiosResponse<ApiDataType>> => {
+  try {
+    const todo: Omit<ITodo, "_id"> = {
+      title: formdata.title,
+      description: formdata.description,
+      completed: false,
+    };
+
+    const savedTodo: AxiosResponse<ApiDataType> = await axios.post(
+      baseUrl + "/api/todos",
+      todo
+    );
+
+    return savedTodo;
+  } catch (error) {
+    throw new Error();
+  }
+};
