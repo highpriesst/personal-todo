0f81bb4779d5e2836eb8703577714f5a60fa01e1
highpriesst <ertansofia@gmail.com>
2023-04-04 00:12:09 +0300

    Changed to h1, styled it.

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,8 @@ import TodoList from "./components/TodoList";
 function App() {
   return (
     <div className="App flex items-center justify-center flex-col">
-      <h2>TODO APP</h2>
-      <div className="flex gap-10">
+      <h1 className="font-bold text-3xl pb-10">Todo App</h1>
+      <div className="flex gap-10 justify-evenly">
         <TodoForm />
         <TodoList />
       </div>
