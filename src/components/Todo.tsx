cdb2a211d5e06bba5174e34c8678d0d0759d1313
highpriesst <ertansofia@gmail.com>
2023-04-04 18:08:01 +0300

    Updated the components according to new functionalty in hooks, utils.

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -58,16 +58,17 @@ import { ITodo } from "../types/todo.types";
 interface ITodoProps {
   index: number;
   todo: ITodo;
-  updateTodo: (todo: ITodo) => void;
+  handleUpdateTodo: (todo: ITodo) => void;
 }
 
-function Todo({ todo, index, updateTodo }: ITodoProps) {
+function Todo({ todo, index, handleUpdateTodo }: ITodoProps) {
+  const isCompleted: string = todo.completed ? `line-through` : ``;
   return (
     <div>
       <ul className="flex mb-4 items-center">
         <li key={index}>
           <span>Title: {todo.title}</span>
-          <span>Description: {todo.description}</span>
+          <span className={isCompleted}>Description: {todo.description}</span>
           <span>
             Completed: {todo.completed === true ? `Finished` : `Not finished`}
           </span>
@@ -78,11 +79,35 @@ function Todo({ todo, index, updateTodo }: ITodoProps) {
               ? `text-green-500 border-green-500 hover:bg-green-700`
               : `text-gray-500 border-gray-500 hover:bg-gray-700`
           }`}
-          onClick={() => updateTodo(todo)}>
-          Done
+          onClick={() => handleUpdateTodo(todo)}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6">
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M4.5 12.75l6 6 9-13.5"
+            />
+          </svg>
         </button>
         <button className="flex-no-shrink p-2 ml-2 border-2 rounded text-red-500 border-red-500 hover:text-white hover:bg-red-700">
-          Remove
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6">
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M6 18L18 6M6 6l12 12"
+            />
+          </svg>
         </button>
       </ul>
     </div>
--- src/components/todoList.tsx
+++ src/components/todoList.tsx
@@ -1,34 +1,9 @@
-// import useTodos from "../hooks/useTodos";
-// import Todo from "./Todo";
-
-// function TodoList() {
-//   const { todos, error, updateTodo } = useTodos();
-//   return (
-//     <div>
-//       <div>
-//         <h1 className="font-bold text-xl">Todays work</h1>
-//       </div>
-//       {error ? (
-//         <div>{error}</div>
-//       ) : (
-//         <div>
-//           {todos.map((todo, index) => {
-//             return <Todo todo={todo} index={index} updateTodo={updateTodo} />;
-//           })}
-//         </div>
-//       )}
-//     </div>
-//   );
-// }
-
-// export default TodoList;
-
 import useTodos from "../hooks/useTodos";
 import { updateTodo } from "../utils/helper";
 import Todo from "./Todo";
 
 function TodoList() {
-  const { todos, error } = useTodos();
+  const { todos, error, handleUpdateTodo } = useTodos();
 
   return (
     <div>
@@ -45,7 +20,7 @@ function TodoList() {
                 key={index}
                 index={index}
                 todo={todo}
-                updateTodo={updateTodo}
+                handleUpdateTodo={handleUpdateTodo}
               />
             );
           })}
