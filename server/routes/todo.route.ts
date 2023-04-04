8b18618b5ceb6f3d7ecab8503258030fb5c292f1
highpriesst <ertansofia@gmail.com>
2023-04-04 17:37:29 +0300

    New route edit/:id added

--- server/routes/todo.route.ts
+++ server/routes/todo.route.ts
@@ -1,10 +1,12 @@
 import express, { Router } from "express";
-import { getTodos, addTodo } from "../controllers/todo.controller";
+import { getTodos, addTodo, updateTodo } from "../controllers/todo.controller";
 
 const router: Router = express.Router();
 
 router.get("/api/todos", getTodos);
 
 router.post("/api/todos", addTodo);
+
+router.put("/edit/:id", updateTodo);
 
 export default router;
