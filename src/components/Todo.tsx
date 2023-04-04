a2155086869efe054a2cbefd97ce911707c97ed9
highpriesst <ertansofia@gmail.com>
2023-04-04 16:23:42 +0300

    Changed the form todo list item's style.

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,36 +1,18 @@
 import { ITodoProps } from "../types/todo.types";
 
-const colorVariants = {
-  notFinished:
-    "flex-no-shrink p-2 ml-4 mr-2 border-2 rounded text-grey-500 border-grey-500",
-  finished:
-    "flex-no-shrink p-2 ml-4 mr-2 border-2 rounded text-green-500 border-green-500",
-  remove:
-    "flex-no-shrink p-2 ml-2 border-2 rounded text-red-500 border-red-500 hover:bg-red",
-};
-
 function Todo({ title, description, index, completed }: ITodoProps) {
   return (
-    <div className="p-1">
-      <ul className="flex mb-4 items-center text-black p-3">
-        <li key={index}>
-          <span>Title {title}</span>
-          <span className={completed ? `line-through text-green-500` : ""}>
-            Description {description}
-          </span>
-          <span>
-            Completed {completed === true ? `Finished` : `Not finished`}
-          </span>
-        </li>
-        <button
-          disabled={!completed}
-          className={
-            completed ? colorVariants.finished : colorVariants.notFinished
-          }>
-          Done
-        </button>
-        <button className={colorVariants.remove}>Remove</button>
-      </ul>
+    <div className="mb-5 flex items-center">
+      <button className="w-12 h-12 mr-3">!</button>
+      <div className="flex-1">
+        <ul className="flex gap-3 text-center justify-center">
+          <li key={index} className="flex flex-col">
+            <span>Title {title}</span>
+            <span>Description {description}</span>
+          </li>
+        </ul>
+      </div>
+      <button className="w-12 h-12 ml-3">X</button>
     </div>
   );
 }
