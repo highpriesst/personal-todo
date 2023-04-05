22bdac865ce251fc6dfd5672506c0ba1e36ab413
highpriesst <ertansofia@gmail.com>
2023-04-05 20:09:40 +0300

    Changed styling and some methods to work better

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,117 +1,39 @@
-// import { ITodo } from "../types/todo.types";
-
-// type Props = ITodo & {
-//   updateTodo: (todo: ITodo) => void;
-//   deleteTodo: (_id: string) => void;
-// };
-
-// function Todo(todos: Props, index: string) {
-//   return (
-//     <div className="mb-5 flex items-center">
-//       <button className="w-12 h-12 mr-3" onClick={() => updateTodo(todos)}>
-//         <svg
-//           xmlns="http://www.w3.org/2000/svg"
-//           fill="none"
-//           viewBox="0 0 24 24"
-//           strokeWidth={1.5}
-//           stroke="currentColor"
-//           className="w-6 h-6">
-//           <path
-//             strokeLinecap="round"
-//             strokeLinejoin="round"
-//             d="M4.5 12.75l6 6 9-13.5"
-//           />
-//         </svg>
-//       </button>
-//       <div className="flex-1">
-//         <ul className="flex gap-3 text-center justify-center">
-//           <li key={index} className="flex flex-col">
-//             <span>Title {todos.title}</span>
-//             <span>Description {todos.description}</span>
-//           </li>
-//         </ul>
-//       </div>
-//       <button className="w-12 h-12 ml-3">
-//         <svg
-//           xmlns="http://www.w3.org/2000/svg"
-//           fill="none"
-//           viewBox="0 0 24 24"
-//           strokeWidth={1.5}
-//           stroke="currentColor"
-//           className="w-6 h-6">
-//           <path
-//             strokeLinecap="round"
-//             strokeLinejoin="round"
-//             d="M6 18L18 6M6 6l12 12"
-//           />
-//         </svg>
-//       </button>
-//     </div>
-//   );
-// }
-
-// export default Todo;
-
 import React from "react";
-import { ITodo } from "../types/todo.types";
+import { TodoProps, ITodo } from "../types/todo.types";
 
-interface ITodoProps {
-  index: number;
-  todo: ITodo;
+type Props = TodoProps & {
   handleUpdateTodo: (todo: ITodo) => void;
-}
+};
 
-function Todo({ todo, index, handleUpdateTodo }: ITodoProps) {
-  const isCompleted: string = todo.completed ? `line-through` : ``;
+const Todo: React.FC<Props> = ({ todo, handleUpdateTodo }) => {
+  const checkTodo: string = todo.completed ? `line-through` : "";
+  const buttonColor: string = todo.completed ? `bg-gray-300` : `bg-green-500`;
+
   return (
-    <div>
-      <ul className="flex mb-4 items-center">
-        <li key={index}>
-          <span>Title: {todo.title}</span>
-          <span className={isCompleted}>Description: {todo.description}</span>
-          <span>
-            Completed: {todo.completed === true ? `Finished` : `Not finished`}
-          </span>
-        </li>
+    <div className="flex justify-between items-center bg-white rounded-md shadow-md p-4 mb-4">
+      <div className="flex-grow">
+        <h1 className={`${checkTodo} text-xl font-semibold mb-2`}>
+          {todo.title}
+        </h1>
+        <span className={`${checkTodo} text-gray-600`}>{todo.description}</span>
+      </div>
+      <div className="flex">
         <button
-          className={`flex-no-shrink p-2 ml-4 mr-2 border-2 rounded hover:text-white ${
-            todo.completed === true
-              ? `text-green-500 border-green-500 hover:bg-green-700`
-              : `text-gray-500 border-gray-500 hover:bg-gray-700`
-          }`}
-          onClick={() => handleUpdateTodo(todo)}>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="w-6 h-6">
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M4.5 12.75l6 6 9-13.5"
-            />
-          </svg>
+          onClick={() => handleUpdateTodo(todo)}
+          className={`text-white rounded-md px-4 py-2 ${buttonColor} ${
+            todo.completed ? `hide-button` : ""
+          }`}>
+          Done
         </button>
-        <button className="flex-no-shrink p-2 ml-2 border-2 rounded text-red-500 border-red-500 hover:text-white hover:bg-red-700">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="w-6 h-6">
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M6 18L18 6M6 6l12 12"
-            />
-          </svg>
+        <button
+          className="text-white rounded-md px-4 py-2 bg-red-500 ml-2"
+          // onClick={() => deleteTodo(todo._id)}
+        >
+          Delete
         </button>
-      </ul>
+      </div>
     </div>
   );
-}
+};
 
 export default Todo;
--- src/components/TodoForm.tsx
+++ src/components/TodoForm.tsx
@@ -1,75 +1,67 @@
-import useTodoForm from "../hooks/useFormData";
-import axios from "axios";
-import { TodoFormData } from "../types/todo.types";
-import { useState } from "react";
+import React, { FormEvent, useState } from "react";
+import { ITodoForm } from "../types/todo.types";
 
-const defaultTodoFormData: TodoFormData = {
-  title: "",
-  description: "",
-  completed: false,
-};
+interface IFormProps {
+  handleAddTodo: (e: FormEvent<Element>, formData: ITodoForm) => void;
+}
 
-const TodoForm = () => {
-  const { formData, handleChange, handleSubmit } = useTodoForm();
+const TodoForm: React.FC<IFormProps> = ({ handleAddTodo }) => {
+  const [formData, setFormData] = useState<ITodoForm>({
+    title: "",
+    description: "",
+  });
+
+  function handleFormData(e: React.ChangeEvent<HTMLInputElement>) {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
+  }
+
+  function handleSubmit(e: FormEvent) {
+    e.preventDefault();
+    setFormData({
+      title: "",
+      description: "",
+    });
+  }
 
   return (
-    <div className="border p-5">
-      <form onSubmit={handleSubmit}>
-        <div className="flex flex-col [&>*]:p-1">
-          <label
-            htmlFor="title"
-            className="block text-sm font-medium leading-6 text-gray-900">
-            Todo
-          </label>
-
-          <input
-            type="text"
-            name="title"
-            id="description"
-            placeholder="Type here..."
-            value={formData.title}
-            onChange={handleChange}
-            className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-          />
-
-          <label
-            htmlFor="description"
-            className="block text-sm font-medium leading-6 text-gray-900">
-            Description
-          </label>
-
-          <input
-            type="text"
-            name="description"
-            id="description"
-            value={formData.description}
-            onChange={handleChange}
-            className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-          />
-
-          <div>
-            <input
-              type="checkbox"
-              name="completed"
-              id="completed"
-              checked={formData.completed}
-              onChange={handleChange}
-            />
-            <label
-              htmlFor="completed"
-              className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300 float-right">
-              Completed
-            </label>
-          </div>
-
-          <button
-            type="submit"
-            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-            Add
-          </button>
-        </div>
-      </form>
-    </div>
+    <form
+      className="flex flex-col gap-4"
+      onSubmit={(e) => handleAddTodo(e, formData)}>
+      <div className="flex flex-col">
+        <label htmlFor="title" className="text-lg font-bold mb-2">
+          Title
+        </label>
+        <input
+          onChange={handleFormData}
+          type="text"
+          id="title"
+          value={formData.title}
+          className="border rounded py-2 px-3"
+        />
+      </div>
+      <div className="flex flex-col">
+        <label htmlFor="description" className="text-lg font-bold mb-2">
+          Description
+        </label>
+        <input
+          onChange={handleFormData}
+          type="text"
+          id="description"
+          value={formData.description}
+          className="border rounded py-2 px-3"
+        />
+      </div>
+      <button
+        type="submit"
+        disabled={formData === undefined ? true : false}
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        onClick={handleSubmit}>
+        Add Todo
+      </button>
+    </form>
   );
 };
 
